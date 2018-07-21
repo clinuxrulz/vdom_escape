0ca43c002ba27f19ef7ddada7780c9b9392f5253
Clinton Selke <clinuxrulz@gmail.com>
2018-07-21 12:04:01 +1000

    Atomic and IncF.constant

---
+++ src/main/java/org/highj/vdom_escape/Atomic.java
@@ -0,0 +1,22 @@
+package org.highj.vdom_escape;
+
+/**
+ * Atomic incremental values, the change structure of Atomic A is just Maybe A.
+ * This is useful for primitive types, like int, double or string.
+ * @param <A>
+ */
+public class Atomic<A> {
+    private final A _value;
+
+    private Atomic(A value) {
+        this._value = value;
+    }
+
+    public static <A> Atomic<A> create(A value) {
+        return new Atomic<>(value);
+    }
+
+    public A value() {
+        return _value;
+    }
+}
--- src/main/java/org/highj/vdom_escape/IncF.java
+++ src/main/java/org/highj/vdom_escape/IncF.java
@@ -1,6 +1,7 @@
 package org.highj.vdom_escape;
 
 import org.derive4j.hkt.__2;
+import org.highj.data.Maybe;
 import org.highj.data.tuple.T2;
 import org.highj.vdom_escape.inc_f.IncFCartesian;
 import org.highj.vdom_escape.inc_f.IncFCategory;
@@ -10,6 +11,21 @@ public interface IncF<A,B> extends __2<IncF.µ,A,B> {
     final class µ {}
 
     T2<B,IncF2<A,B>> apply(A a);
+
+    static <A,B> IncF<A,Atomic<B>> constant(Atomic<B> b) {
+        class Util {
+            private IncF2<A,Atomic<B>> constant2 = null;
+        }
+        final Util util = new Util();
+        util.constant2 = (Delta<A,?> da) -> T2.of(
+            Delta.<Atomic<B>,Maybe<B>>create(
+                (b2, db) -> Atomic.create(db.getOrElse(b2.value())),
+                Maybe.Nothing()
+            ),
+            util.constant2
+        );
+        return (A a) -> T2.of(b, util.constant2);
+    }
 
     IncFSemigroupoid semigroupoid = new IncFSemigroupoid() {};
 
