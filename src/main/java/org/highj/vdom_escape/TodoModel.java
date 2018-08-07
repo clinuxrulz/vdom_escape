0e1f311088dac02fb287b8d0a174a37532e5000d
Clinton Selke <clinuxrulz@gmail.com>
2018-08-07 20:45:58 +1000

    TodoModel::incFGetTasks

--- src/main/java/org/highj/vdom_escape/TodoModel.java
+++ src/main/java/org/highj/vdom_escape/TodoModel.java
@@ -1,5 +1,8 @@
 package org.highj.vdom_escape;
 
+import org.highj.data.Maybe;
+import org.highj.data.tuple.T2;
+
 public class TodoModel {
     private final IMap<Integer,Atomic<String>> _tasks;
 
@@ -15,5 +18,33 @@ public class TodoModel {
 
     public TodoModel patch(TodoModelChanges changes) {
         return new TodoModel(tasks().patch(changes.taskChanges()));
+    }
+
+    public static Delta<TodoModel,TodoModelChanges> materializeDelta(Delta<TodoModel,?> delta) {
+        //noinspection unchecked
+        return (Delta)delta;
+    }
+
+    public static IncF<TodoModel,IMap<Integer,Atomic<String>>> incFGetTasks() {
+        return (TodoModel todoModel) -> {
+            class Util {
+                private IncF2<TodoModel,IMap<Integer,Atomic<String>>> iGetTasks2;
+            }
+            final Util util = new Util();
+            util.iGetTasks2 = (Delta<TodoModel, ?> da) ->
+                T2.of(
+                    Delta.create(
+                        IMap::patch,
+                        materializeDelta(da)
+                            .change()
+                            .taskChanges()
+                    ),
+                    util.iGetTasks2
+                );
+            return T2.of(
+                todoModel.tasks(),
+                util.iGetTasks2
+            );
+        };
     }
 }
